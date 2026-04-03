dbc736e347c08b8025d0d1aca07fc1402e2d3f32
CEO <wo1314521ya@outlook.com>
2026-04-03 19:54:15 +0800

    Day 14

--- api.py
+++ api.py
@@ -7,12 +7,22 @@ from service import StudentService
 app = FastAPI(title="Student Manager API")
 service = StudentService()
 
+##添加学生请求体：
 class StudentCreateRequest(BaseModel):
     id:str
     name:str
     age:int
     major:str
     score:int
+
+
+##相当于我制定了规制如下(修改学生接口必须这样输入）：
+class StudentUpdateRequest(BaseModel):
+    name:str
+    age:int
+    major:str
+    score:int
+
 
 def student_to_dict(student):
     return {
@@ -58,4 +68,22 @@ async def create_student(student_data:StudentCreateRequest):
         return student_to_dict(student)
     except(ValueError,TypeError)as e:
         raise HTTPException(status_code=400,detail=str(e))
-        
+
+##修改学生接口：
+@app.put("/student{student_id}")
+async def update_student(student_id:str, student_data:StudentUpdateRequest):
+    existing_student = service.find_student_by_id(student_id)
+    if existing_student is None:
+        raise HTTPException(status_code=404,detail="未找到该学号对应的学生。")
+    try:
+        updated_student = service.update_student_by_id(
+            student_id,
+            student_data.name,
+            student_data.age,
+            student_data.major,
+            student_data.score,
+            )
+        return student_to_dict(updated_student)
+    except (ValueError, TypeError) as e:
+        raise HTTPException (status_code=400,detail=str(e))
+         
