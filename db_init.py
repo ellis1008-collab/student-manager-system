7690e6a6e643e4e113fcc8086b8f9e0ee31326e3
CEO <wo1314521ya@outlook.com>
2026-04-09 14:00:08 +0800

    Day 20: initialize SQLite database and students table

---
+++ db_init.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("data/student.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE 
+    IF NOT EXISTS students
+    (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    major TEXT NOT NULL,
+    score INTEGER NOT NULL
+    )
+    """)
+    conn.commit()
+    conn.close()
+
+    print("数据库初始化完成。")
+
+if __name__ == "__main__":
+    init_db()    
